988b0be94971656fa2f629ea07e93ca01a4b676d
Akshayaks1999 <103184869+Akshayaks1999@users.noreply.github.com>
2022-06-02 03:20:20 -0700

    Add files via upload

---
+++ sum1.java
@@ -0,0 +1,19 @@
+class sum1
+{
+int a,b,c;
+void add()
+{
+a=10;
+b=20;
+c=a+b
+}}
+class sum1 extends sum
+{
+system.out.print("sum= "+c);
+}
+public static void main(String args[])
+{
+sum1 s=new sum1();
+s.add();
+s.print();
+}}
